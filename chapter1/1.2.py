2c239a85da8bd1c8e825446cc5111d2d2f3978fd
fernandodr <frcompsci@gmail.com>
2018-10-07 21:48:26 -0400

    Update 1.2.py

--- chapter1/1.2.py
+++ chapter1/1.2.py
@@ -1,16 +1,12 @@
 #assuming that the string entered ends with a \0, which would make it the C-style string
-
-something = raw_input("Enter a string: ")
-
-real_length = (len(something)-2)
-
-print real_length
-
-i = real_length - 1
-reversed = ""
-while(i>=0):
-	reversed = reversed + something[i]
-	i = i - 1
-reversed = reversed + "\\0"
-
-print reversed
+def reverser(something):
+	real_length = (len(something)-2)
+	print real_length
+	i = real_length - 1
+	formatted_string = ""
+	while(i>=0):
+		formatted_string += something[i]
+		i -= 1
+	formatted_string += "\\0"
+	return formatted_string
+	
